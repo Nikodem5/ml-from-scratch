9d2559d43accbaf96cc15b0c4a75349ebb928581
Nikodem5 <lskywalker568@gmail.com>
2024-09-23 13:05:10 +0200

    Created init file in src directory. Made printing iteration and cost info optional.

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -31,7 +31,7 @@ class LinearRegression:
 
         return dj_dw, dj_db
 
-    def gradient_descent(self, X, y, alpha, iters):
+    def gradient_descent(self, X, y, alpha, iters, info=False):
         for i in range(iters):
             dj_dw, dj_db = self.compute_gradient(X, y)
 
@@ -40,13 +40,13 @@ class LinearRegression:
             self.w = w_tmp
             self.b = b_tmp
 
-            if i % (iters//10) == 0:
+            if i % (iters//10) == 0 and info:
                 print(f'iteration: {i} cost: {self.compute_cost(X, y)}')
 
         return self.w, self.b
 
-    def fit(self, X, y, alpha=0.01, iters=10):
+    def fit(self, X, y, alpha=0.01, iters=10, info=False):
         self.m, self.n = X.shape
         self.w = np.random.rand(self.n)
         self.b = 100
-        self.w, self.b = self.gradient_descent(X, y, alpha, iters)
+        self.w, self.b = self.gradient_descent(X, y, alpha, iters, info)

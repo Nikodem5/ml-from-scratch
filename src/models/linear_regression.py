1755f9942abc383c71bbca6472d9f80031cedcf6
Nikodem5 <lskywalker568@gmail.com>
2024-09-23 14:09:43 +0200

    Created y_pred variable to use predict() function just once. Added vectorization to cost function

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -7,33 +7,32 @@ class LinearRegression:
         self.m = None
         self.n = None
 
-    def predict(self, x):
-        return np.dot(self.w, x) + self.b
+    def predict(self, X):
+        return np.dot(X, self.w) + self.b
 
-    def compute_cost(self, X, y):
-        total_error = 0
-        for i in range(self.m):
-            total_error += np.square(self.predict(X[i]) - y[i])
-        total_error = total_error / (2*self.m)
+    def _compute_cost(self, X, y, y_pred):
+        cost = np.sum(np.square(y_pred - y))
+        cost = cost / (2 * self.m)
 
-        return total_error
+        return cost
 
-    def compute_gradient(self, X, y):
+    def _compute_gradient(self, X, y, y_pred):
         dj_db = 0
         dj_dw = np.zeros(self.n)
 
         for i in range(self.m):
-            dj_db += self.predict(X[i]) - y[i]
+            dj_db += y_pred[i] - y[i]
             for j in range(self.n):
-                dj_dw[j] += (self.predict(X[i]) - y[i]) * X[i][j]
+                dj_dw[j] += (y_pred[i] - y[i]) * X[i][j]
         dj_db = dj_db / self.m
         dj_dw = dj_dw / self.m
 
         return dj_dw, dj_db
 
-    def gradient_descent(self, X, y, alpha, iters, info=False):
+    def gradient_descent(self, X, y, alpha, iters, info):
         for i in range(iters):
-            dj_dw, dj_db = self.compute_gradient(X, y)
+            y_pred = self.predict(X)
+            dj_dw, dj_db = self._compute_gradient(X, y, y_pred)
 
             w_tmp = self.w - (alpha * dj_dw)
             b_tmp = self.b - (alpha * dj_db)
@@ -41,7 +40,7 @@ class LinearRegression:
             self.b = b_tmp
 
             if i % (iters//10) == 0 and info:
-                print(f'iteration: {i} cost: {self.compute_cost(X, y)}')
+                print(f'iteration: {i} cost: {self._compute_cost(X, y, y_pred)}')
 
         return self.w, self.b
 
@@ -49,4 +48,5 @@ class LinearRegression:
         self.m, self.n = X.shape
         self.w = np.random.rand(self.n)
         self.b = np.random.randint(1, 10)
+        
         self.w, self.b = self.gradient_descent(X, y, alpha, iters, info)

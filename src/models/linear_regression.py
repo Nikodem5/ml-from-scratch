1ca5b9e0f7dc628cfd6c09c77a7dd5d1bd245111
Nikodem5 <lskywalker568@gmail.com>
2024-09-25 13:31:30 +0200

    Improved automatic convergence test (added patience) to avoid problems resulting from cost function fluctuations. Also changed preparations for normalization

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -1,12 +1,16 @@
 import numpy as np
 from ..utils.z_score_normalization import ZScoreScaler
 
+#TODO regularization, normalization, early stopping, adaptive alpha
+
 class LinearRegression:
-    def __init__(self) -> None:
+    def __init__(self, normalization=False) -> None:
         self.w = None
         self.b = None
         self.m = None
         self.n = None
+        self.normalization = normalization
+        self.scaler = ZScoreScaler if normalization else None
 
     def predict(self, X):
         return np.dot(X, self.w) + self.b
@@ -27,7 +31,8 @@ class LinearRegression:
 
         return dj_dw, dj_db
 
-    def gradient_descent(self, X, y, alpha, epsilon, iters, info):
+    def gradient_descent(self, X, y, alpha, epsilon, iters, patience, info):
+        convergance_count = 0
         for i in range(iters):
             y_pred = self.predict(X)
             dj_dw, dj_db = self._compute_gradient(X, y, y_pred)
@@ -39,18 +44,22 @@ class LinearRegression:
 
             cost = self._compute_cost(X, y, y_pred)
             if cost <= epsilon:
-                if info:
-                    print(f'last iteration: {i} cost: {cost}')
-                break
+                convergance_count += 1
+                if convergance_count >= patience:
+                    if info:
+                        print(f'last iteration: {i} cost: {cost}')
+                    break
+            else:
+                convergance_count = 0
 
             if info and (i % (iters//10) == 0 or i + 1 == iters):
                 print(f'iteration: {i} cost: {cost}')
 
         return self.w, self.b
 
-    def fit(self, X, y, alpha=0.01, epsilon=1e-3, iters=10, info=False, normalization=True):
+    def fit(self, X, y, alpha=0.01, epsilon=1e-3, iters=10, patience=5, info=False):
         self.m, self.n = X.shape
         self.w = np.random.rand(self.n)
         self.b = np.random.randint(1, 10)
         
-        self.w, self.b = self.gradient_descent(X, y, alpha, epsilon, iters, info)
+        self.w, self.b = self.gradient_descent(X, y, alpha, epsilon, iters, patience, info)

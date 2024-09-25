c63638e5e2f0382056a907ef23c3e175f3254231
Nikodem5 <lskywalker568@gmail.com>
2024-09-25 13:21:16 +0200

    Added automatic convergance test

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -27,7 +27,7 @@ class LinearRegression:
 
         return dj_dw, dj_db
 
-    def gradient_descent(self, X, y, alpha, iters, info):
+    def gradient_descent(self, X, y, alpha, epsilon, iters, info):
         for i in range(iters):
             y_pred = self.predict(X)
             dj_dw, dj_db = self._compute_gradient(X, y, y_pred)
@@ -37,15 +37,20 @@ class LinearRegression:
             self.w = w_tmp
             self.b = b_tmp
 
+            cost = self._compute_cost(X, y, y_pred)
+            if cost <= epsilon:
+                if info:
+                    print(f'last iteration: {i} cost: {cost}')
+                break
+
             if info and (i % (iters//10) == 0 or i + 1 == iters):
-                cost = self._compute_cost(X, y, y_pred)
                 print(f'iteration: {i} cost: {cost}')
 
         return self.w, self.b
 
-    def fit(self, X, y, alpha=0.01, iters=10, info=False, normalization=True):
+    def fit(self, X, y, alpha=0.01, epsilon=1e-3, iters=10, info=False, normalization=True):
         self.m, self.n = X.shape
         self.w = np.random.rand(self.n)
         self.b = np.random.randint(1, 10)
         
-        self.w, self.b = self.gradient_descent(X, y, alpha, iters, info)
+        self.w, self.b = self.gradient_descent(X, y, alpha, epsilon, iters, info)

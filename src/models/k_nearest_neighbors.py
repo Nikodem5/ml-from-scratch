0c573f51ae0d20b32951af9bdc3f7720ea185c58
Nikodem5 <lskywalker568@gmail.com>
2024-10-12 17:25:15 +0200

    Structural changes

--- src/models/k_nearest_neighbors.py
+++ src/models/k_nearest_neighbors.py
@@ -5,18 +5,21 @@ class k_nearest_neighbors():
         self.m = None
         self.n = None
 
-def euclidean_distance(p, q):
-    sum_ = np. sum(np.square(p - q))
-    dist = np.sqrt(sum_) 
-    return dist
+    def euclidean_distance(self, p, q):
+        sum_ = np. sum(np.square(p - q))
+        dist = np.sqrt(sum_) 
+        return dist
+    
+if __name__ == '__main__':
+    knn = k_nearest_neighbors()
 
+    p1 =np.array([4, 1])
+    p2 = np.array([9, 6])
 
-p1 =np.array([4, 1])
-p2 = np.array([9, 6])
+    print(knn.euclidean_distance(p1, p2)) # 5 * sqrt(2)
+    print(np.multiply(5, np.sqrt(2)))
 
-print(euclidean_distance(p1, p2))
-print(np.multiply(5, np.sqrt(2)))
+    q1 = np.array([5, 5, 5, 5])
+    q2 = np.array([9, 9, 12, 17])
 
-q1 = np.array([5, 5, 5, 5])
-q2 = np.array([9, 9, 12, 17])
-print(euclidean_distance(q1, q2))
+    print(knn.euclidean_distance(q1, q2)) # 15
